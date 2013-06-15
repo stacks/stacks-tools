e405e6bfcd91a15ee1a44f0349ac7d4f399d22f0
Pieter Belmans <pieterbelmans@gmail.com>
2013-06-15 20:42:16 +0200

    Updated the graph creation script
    The functionality should be moved to statistics though

--- graphs.py
+++ graphs.py
@@ -1,5 +1,6 @@
 import graphs.generate
 
+
 print "Generating the force-directed graphs"
 graphs.generate.generateForceDirectedGraphs()
 
@@ -9,4 +10,11 @@ graphs.generate.generateClusterGraphs()
 print "Generating the collapsible graphs"
 graphs.generate.generateCollapsibleGraphs()
 
+#graphs.generate.scatter()
+print "\nClearing the dependencies table"
+graphs.generate.clearDependencies()
+print "Inserting dependencies"
+graphs.generate.insertDependencies()
 
+print "\nCreating various graph related statistics"
+graphs.generate.updateCounts()

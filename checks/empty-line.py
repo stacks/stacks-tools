932ed26abb4e7c84606921108a28e3063091bc35
Aise Johan de Jong <aise.johan.de.jong@gmail.com>
2013-08-20 11:42:30 -0400

    Also check for lines that should be empty...

--- checks/empty-line.py
+++ checks/empty-line.py
@@ -12,23 +12,35 @@ def ok_for_first_line(line):
 	if line.find('\\section{') == 0: return 1
 	if line.find('\\subsection{') == 0: return 1
 	if line.find('\\subsubsection{') == 0: return 1
+	if line.find('\\phantomsection') == 0: return 1
 	if line.find('\\bibliography{my}') == 0: return 1
 	if line.find('\\end{document}') == 0: return 1
 	if line.find('\\title{') == 0: return 1
-	if line.find('\\phantomsection') == 0: return 1
 	if line.find('\\tableofcontents') == 0: return 1
 	if line.find('\\maketitle') == 0: return 1
 	if line.find('\\input{chapters}') == 0: return 1
 	if line.find('\\input{preamble}') == 0: return 1
 	return 0
 
+def previous_should_be_empty(line):
+	if line.find('noindent') >= 0: return 1
+	for env in list_of_labeled_envs:
+		if env == 'equation': continue
+		if line.find('\\begin{' + env) == 0: return 1
+	for part in list_parts:
+		if line.find('\\' + part) == 0: return 1
+	return 0
+
 for name in lijstje:
 	if name == "fdl": continue
+	if name == "coding": continue
 	tex_file = open(path + name + ".tex", 'r')
 	nr = 0
 	previous_empty = 0
 	for line in tex_file:
 		nr = nr + 1
+		if not previous_empty and previous_should_be_empty(line):
+			print name, nr, line
 		if line.find('%') == 0: continue
 		if line.strip() == "":
 			previous_empty = 1

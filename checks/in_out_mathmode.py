cf6b6028f525864281b54b413aaa4ca1d30009b4
Aise Johan de Jong <aise.johan.de.jong@gmail.com>
2014-09-06 19:44:48 -0400

    Add more checks to in_out_mathmode.py
    Neurotic behaviour continues...

--- checks/in_out_mathmode.py
+++ checks/in_out_mathmode.py
@@ -2,6 +2,57 @@ from functions import *
 import config
 from functions import *
 
+commands_outside_math_mode = [\
+'\\noindent',\
+'\\ref{',\
+'\\begin{',\
+'\\end{',\
+'\\label{',\
+'\\item ',\
+'\\item\n',\
+'\\item[',\
+'\\medskip',\
+'\\section{',\
+'\\subsection{',\
+'\\subsubsection{',\
+'\\romannumeral',\
+'\\cite[',\
+'\\cite{',\
+'\\ ',\
+'\\footnote{',\
+'\\it ',\
+'\\bf ',\
+'\\title{',\
+'\\input{chapters}',\
+'\\tableofcontents',\
+'\\maketitle',\
+'\\input{preamble}',\
+'\\phantomsection',\
+'\\href{',\
+'\\url{',\
+'\\hyperref{',\
+'\\hyperref[',\
+'\\bibliography{my}',\
+'\\bibliographystyle{amsalpha}',\
+'\\emph{',\
+'\\em ',\
+'\\\'e',\
+'\\`e',\
+'\\\'E',\
+'\\v C',\
+'\\`a',\
+'\\\"a',\
+'\\\"e',\
+'\\\"o',\
+'\\& ',\
+'\\% ',\
+'\\S ']
+
+def command_allowed_outside_math_mode(line, n):
+	for command in commands_outside_math_mode:
+		if line.find(command, n) == n:
+			return 1
+	return 0
 
 def check_line(line, m, name, nr):
 	n = 0
@@ -11,8 +62,10 @@ def check_line(line, m, name, nr):
 		else:
 			if (m == 1) and line.find('footnote', n) == n:
 				print_error('Footnote in math mode', line, name, nr)
-			if (m == 0) and line.find('textit', n) == n:
-				print_error('\\textit in text', line, name, nr)
+			if (m == 0):
+				if line.find('\\', n) == n:
+					if not command_allowed_outside_math_mode(line, n):
+						print_error('command in text', line, name, nr)
 		n = n + 1
 	return m
 
@@ -21,6 +74,7 @@ path = config.localProject + "/"
 lijstje = list_text_files(path)
 
 in_math_mode = 0
+in_display_math_mode = 0
 
 for name in lijstje:
 	if name == "fdl": continue
@@ -31,7 +85,10 @@ for name in lijstje:
 		nr = nr + 1
 		if line.find('$$') == 0 or line.find('{equation}') >= 0 or line.find('{align}') >= 0 or line.find('{align*}') >= 0 or line.find('{eqnarray}') >= 0 or line.find('{eqnarray*}') >= 0:
 			in_math_mode = 1 - in_math_mode
+                        in_display_math_mode = 1 - in_display_math_mode
 			continue
+                if in_display_math_mode and line.find('$') >= 0:
+			print_error('Dollar in display math', line, name, nr)
 		in_math_mode = check_line(line, in_math_mode, name, nr)
 	
 	if in_math_mode == 1:

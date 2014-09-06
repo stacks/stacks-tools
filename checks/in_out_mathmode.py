55a61a75b695e324b32c9d61fff9f25156983745
Aise Johan de Jong <aise.johan.de.jong@gmail.com>
2014-09-06 12:15:28 -0400

    Check for textit in non-math-mode
    We can add many more things to check for here

--- checks/in_out_mathmode.py
+++ checks/in_out_mathmode.py
@@ -3,13 +3,16 @@ import config
 from functions import *
 
 
-def check_footnote(line, m, name, nr):
+def check_line(line, m, name, nr):
 	n = 0
 	while n < len(line):
 		if line[n] == '$':
 			m = 1 - m
-		elif (m == 1) and line.find('footnote', n) == n:
-			print_error('Footnote in math mode', line, name, nr)
+		else:
+			if (m == 1) and line.find('footnote', n) == n:
+				print_error('Footnote in math mode', line, name, nr)
+			if (m == 0) and line.find('textit', n) == n:
+				print_error('\\textit in text', line, name, nr)
 		n = n + 1
 	return m
 
@@ -26,10 +29,10 @@ for name in lijstje:
 	nr = 0
 	for line in tex_file:
 		nr = nr + 1
-		if line.find('$$') == 0:
+		if line.find('$$') == 0 or line.find('{equation}') >= 0 or line.find('{align}') >= 0 or line.find('{align*}') >= 0 or line.find('{eqnarray}') >= 0 or line.find('{eqnarray*}') >= 0:
 			in_math_mode = 1 - in_math_mode
 			continue
-		in_math_mode = check_footnote(line, in_math_mode, name, nr)
+		in_math_mode = check_line(line, in_math_mode, name, nr)
 	
 	if in_math_mode == 1:
 		print_error('End chapter in math mode', line, name, nr)

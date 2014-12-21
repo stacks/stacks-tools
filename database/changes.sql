5a0e47f1a2c31dddef5f838b4f42d933b35532ed
Pieter Belmans <pieterbelmans@gmail.com>
2014-12-21 21:36:50 +0100

    Some changes to `database/changes.sql`
    This should've been in two commits ago.

--- database/changes.sql
+++ database/changes.sql
@@ -1,8 +1,9 @@
 CREATE TABLE "changes" (
   "tag" VARCHAR NOT NULL,
-  "commit" VARCHAR NOT NULL,
+  "hash" VARCHAR NOT NULL,
   "file" VARCHAR NOT NULL,
+  "type" VARCHAR NOT NULL,
+  "label" VARCHAR NOT NULL,
   "begin" INTEGER,
-  "end" INTEGER,
-  "type" VARCHAR
+  "end" INTEGER
 );

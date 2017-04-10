2b71826bad9417babf32635f61e5add62c05f4fd
Aise Johan de Jong <aise.johan.de.jong@gmail.com>
2017-04-09 20:50:39 -0400

    New chapter added to functions and fix_title_refs

--- checks/fix_titles_refs.py
+++ checks/fix_titles_refs.py
@@ -96,7 +96,8 @@ short_titles = {'introduction' : 'Introduction',\
 'stacks-perfect' : 'Derived Categories of Stacks,',\
 'stacks-introduction' : 'Introducing Algebraic Stacks,',\
 'stacks-more-morphisms' : 'More on Morphisms of Stacks,',\
-'moduli' : 'Moduli of Curves,',\
+'moduli' : 'Moduli Stacks,',\
+'moduli-curves' : 'Moduli of Curves,',\
 'examples' : 'Examples,',\
 'exercises' : 'Exercises,',\
 'guide' : 'Guide to Literature,',\
